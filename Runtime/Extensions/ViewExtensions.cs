3d5af2a65d8b842e55b4707d129c61e7e47b3373
David Orbelian <david.orbelian@gmail.com>
2020-05-12 13:48:46 +0400

    Add Runtime View Extensions.

---
+++ Runtime/Extensions/ViewExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Gamgaroo.MVVM.Runtime.Abstractions;
+using Gamgaroo.Reactive.Runtime.Abstractions;
+
+namespace Gamgaroo.MVVM.Runtime.Extensions
+{
+    public static class ViewExtensions
+    {
+        public static void Bind<T>(this IView<T> view, IReactiveProperty<T> property)
+        {
+            if (property != null)
+            {
+                property.OnValueChanged += view.Set;
+                view.Set(property.Value);
+            }
+            else
+            {
+                view.Set(default);
+            }
+        }
+
+        public static void Unbind<T>(this IView<T> view, IReactiveProperty<T> property)
+        {
+            property.OnValueChanged -= view.Set;
+            view.Set(default);
+        }
+    }
+}
