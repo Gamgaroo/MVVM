da2b45e812b5557fa4303a60261cf91ed93d8922
David Orbelian <david.orbelian@gmail.com>
2020-05-26 18:24:29 +0400

    Add specific IView interfaces for TextView and TextMeshProView to bind that views to appropriate properties.

--- Runtime/Views/Primitives/TextMeshProView.cs
+++ Runtime/Views/Primitives/TextMeshProView.cs
@@ -3,16 +3,21 @@ using TMPro;
 
 namespace Gamgaroo.MVVM.Runtime.Views.Primitives
 {
-    public class TextMeshProView : TextMeshProUGUI, IView<string>, IView<object>
+    public class TextMeshProView : TextMeshProUGUI, IView<string>, IView<float>, IView<int>
     {
-        public void Set(object viewModel)
+        public void Set(string viewModel)
+        {
+            text = viewModel;
+        }
+
+        public void Set(float viewModel)
         {
             text = viewModel.ToString();
         }
 
-        public void Set(string viewModel)
+        public void Set(int viewModel)
         {
-            text = viewModel;
+            text = viewModel.ToString();
         }
     }
 }
